19cff060aa13e35eda014bbd8e85ff8fcf2995d2
AftabSoftDev <almakhdoom078692@gmail.com>
2025-02-23 22:25:30 +0530

    set routes & validation file generate

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function getProducts(Request $req)
+    {
+        if ($req->isMethod('GET')) {
+            return $req->all();
+            if (isset($id) && !empty($id) && is_numeric($id) && $id > 0) {
+                return $id;
+            } else {
+                return "No product ID provided";
+            }
+        } else {
+            return http_response_code(405);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/products',);
+Route::prefix('/v1')->group(function () {
+    Route::controller(ProductController::class)->group(function () {
+        Route::get('products/{id}', 'getProducts');
+        Route::get('products/{category_id?}', 'getProducts');
+        Route::post('products/', 'getProducts');
+        Route::put('products/{id?}', 'getProducts');
+        Route::delete('products/{id?}', 'getProducts');
+    });
+});
